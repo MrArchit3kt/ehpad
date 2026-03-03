b79d127c2e6acb61e716a2999a9c33d67d4fc58e
Dylan <mrdettlingdylan@gmail.com>
2026-03-03 01:19:19 +0100

    Disable legacy site access session check

--- src/server/access/session.ts
+++ src/server/access/session.ts
@@ -1,15 +1,3 @@
-import { cookies } from "next/headers";
-import { ACCESS_COOKIE_NAME } from "@/server/access/constants";
-
 export async function hasValidSiteAccess() {
-  const cookieStore = await cookies();
-  const accessCookie = cookieStore.get(ACCESS_COOKIE_NAME)?.value;
-  const expected = process.env.SITE_ACCESS_PASSWORD;
-
-  if (!expected) {
-    console.warn("SITE_ACCESS_PASSWORD is not set");
-    return false;
-  }
-
-  return accessCookie === expected;
+  return true;
 }
